b9932dcf176e2512501d4bf787b2857797b682d1
陈云杰 <chenyunjie@qianmi.com>
2017-11-01 11:34:58 +0800

    D-

---
+++ arenakitsupport/src/main/java/com/bm001/arena/arenakitsupport/picker/view/OnItemSelectedListener.java
@@ -0,0 +1,6 @@
+package com.bm001.arena.arenakitsupport.picker.view;
+
+
+public interface OnItemSelectedListener {
+    void onItemSelected(String item, int index);
+}
---
+++ arenakitsupport/src/main/java/com/bm001/arena/arenakitsupport/picker/view/OnSelectedListener.java
@@ -0,0 +1,13 @@
+package com.bm001.arena.arenakitsupport.picker.view;
+
+import java.util.ArrayList;
+
+/**
+ * Created by heng on 16/9/6.
+ */
+
+public interface OnSelectedListener {
+
+    void onSelected(ArrayList<ReturnData> selectedList);
+
+}
